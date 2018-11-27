93af273b1d7fb156786ee4b3697fb16f0efb6f23
serapath <dev@serapath.de>
2018-11-27 10:56:26 +0000

    ADD fixed compiler + fullscreen default dark theme (first draft)

--- src/play-editor.js
+++ src/play-editor.js
@@ -5,17 +5,19 @@ const solcjs = require('solc-js')
 const smartcontract = require('smartcontract-app')
 const codingeditor = require('coding-editor')
 
-const tiler = require('tiler')
-
+const twm = require('twm')
 const menubar = require('menubar')
+
+const defaultTheme = require('./theme.js')
 
 module.exports = playeditor
 
-function playeditor (opts = {}) {
+function playeditor (opts = {}, theme = defaultTheme) {
   const ed = {
-    name: 'contract.sol',
+    name: opts.name || 'contract.sol',
     el: codingeditor({
-      value: `
+      value: opts.value || `
+      pragma solidity ^0.4.17;
 contract Mortal {
     /* Define variable owner of the type address */
     address owner;
@@ -43,7 +45,7 @@ contract Greeter is Mortal {
 }
       `,
       lineNumbers: true,
-    }),
+    }, theme),
   }
   var compiler
   solcjs.version2url((err, select) => {
@@ -58,45 +60,103 @@ contract Greeter is Mortal {
   function update () {
     if (compiler) {
       var sourcecode = ed.el.api.getValue()
-      var id = setTimeout(() => {
-        var metadata = compiler.compile(sourcecode)
-        // console.log(metadata)
-        var el = smartcontract({ metadata })
+      var id = setTimeout(async () => {
+        var result = await compiler(sourcecode)
+        var opts = {
+          metadata: {
+            compiler: { version: result[0].compiler.version },
+            language: result[0].compiler.language,
+            output: {
+              abi: JSON.parse(result[0].abi),
+              devdoc: result[0].metadata.devdoc,
+              userdoc: result[0].metadata.userdoc
+            },
+            settings: {
+              compilationTarget: { '': result[0].sources.compilationTarget },
+              evmVersion: result[0].compiler.evmVersion,
+              libraries: result[0].sources.libraries,
+              optimizer: { enabled: result[0].compiler.optimizer, runs: result[0].compiler.runs },
+              remapings: result[0].sources.remappings
+            },
+            sources: { '': result[0].sources.sourcecode }
+          }
+        }
+        var el = smartcontract(opts)
         scapp.el.innerHTML = ''
         scapp.el.appendChild(el)
-        output.el.textContent = JSON.stringify(metadata)
+        output.el.textContent = JSON.stringify(opts)
+        console.log('dosomething! ...maybe')
+        setTimeout(() => ed.el.api.resize('auto'))
+        window.EDITOR = ed.el.api
       }, 0)
     }
   }
   ed.el.api.on('change', debounce((api) => update()))
-  function debounce (fn) {
-    const wait = 100
-    var timeout, context, args
-    const exec = () => {
-      fn.apply(context, args)
-      timeout = undefined
-    }
-    return function () {
-      context = this
-      args = arguments
-      if (timeout) return
-      timeout = setTimeout(exec, wait)
-    }
-  }
-  const out = { el: bel`<p>OUTPUT</b>`, name: 'output' }
-  const sc = { el: bel`<div></div>`, name: 'preview' }
-  const mb = { el: menubar(), name: 'navbar' }
-  const mosaic = tiler`
-    [${ed}] ${out} | ${sc}
+  const out = { el: bel`<p>OUTPUT</b>`, name: 'output.json' }
+  const sc = { el: bel`<div style="height:100%;"></div>`, /*name: 'preview'*/ }
+  var items = [
+    { title: 'editor',
+        fn: e => {
+        mosaic.api({ cmd: 'show', target: editor })
+        mosaic.api({ cmd: 'hide', target: scapp })
+        var height = ed.el.getBoundingClientRect().height
+        var width = window.innerWidth
+        ed.el.api.resize({ width, height })
+      }
+    },
+    { active: true, title: 'both',
+      fn: e => {
+        mosaic.api({ cmd: 'show', target: editor })
+        mosaic.api({ cmd: 'show', target: scapp })
+        var height = ed.el.getBoundingClientRect().height
+        var width = window.innerWidth / 2
+        ed.el.api.resize({ width, height })
+      }
+    },
+    { title: 'preview',
+        fn: e => {
+        mosaic.api({ cmd: 'hide', target: editor })
+        mosaic.api({ cmd: 'show', target: scapp })
+        var height = 0 // ed.el.getBoundingClientRect().height
+        var width = 0 // window.innerWidth / 2
+        ed.el.api.resize({ width, height })
+      }
+    },
+  ]
+  const mb = { el: menubar({ items, theme }) }
+  const mosaic = (theme !== defaultTheme ? twm(theme) : used++ ? twm : twm(theme))`
+    [[${ed}]] ${out} | ${sc}
     ${mb}`
   const [,[,[,editor, output],[,scapp]],[,navbar]] = Array.from(mosaic)
-  return bel`<div class=${css.playeditor}>${mosaic}</div>`
+  return mosaic
+  // bel`<div class=${css.playeditor}>${mosaic}</div>`
 }
+var used = 0
 
 const css = csjs`
   .playeditor {
+    box-sizing: border-box;
     background-color: pink;
+    border: 5px solid red;
+    flex-grow: 1;
     height: 100%;
     width: 100%;
   }
 `
+/******************************************************************************
+  HELPER
+******************************************************************************/
+function debounce (fn) {
+  const wait = 100
+  var timeout, context, args
+  const exec = () => {
+    fn.apply(context, args)
+    timeout = undefined
+  }
+  return function () {
+    context = this
+    args = arguments
+    if (timeout) return
+    timeout = setTimeout(exec, wait)
+  }
+}
---
+++ src/theme.js
@@ -0,0 +1,107 @@
+// @TODO: add two classes to components
+// 1. invariant class (layout, etc...)
+// 2. custom class with default class fallback (for customisation)
+// 3. make it "live updatable" (classes)
+// 4. enable constraints inside "custom" (e.g. css variables?)
+// 5. enable updates to (e.g. css variables)
+
+var tabfont = 'Courier New'  // mono spaced
+var smokewhite = '#f5f5f5'   // background light
+var slateGrey = '#8a929b'    // input background text
+var lavenderGrey = '#e3e8ee' // inputs background
+var white = '#ffffff'        // borders & font on input background
+var darkgrey = '#21252b'     // separators
+
+var dark = '#2c323c'         // background dark
+var darkish = 'hsla(0, 0%, 8%, 1)' // dark seperator
+// var violetRed = '#b25068'    // used as red in types (bool etc.)
+// var aquaMarine = '#90FCF9'   // used as green in types (bool etc.)
+// var turquoise = '#14b9d5'
+// var yellow = '#F2CD5D'
+// var androidGreen = '#9BC53D'
+
+const lighttheme = {
+  // TWM ------------------------------
+  font1: tabfont,        // tab font
+  color_text: darkgrey,        // tab inactive text
+  color_activeText: white,     // tab active text
+  color_bgPanel: smokewhite,    // panel background & separators
+  color_bgTab: lavenderGrey,  // tab
+  color_bgContent: lavenderGrey,  // TAB BACKGROUND - lavenderGrey -
+  color_background: slateGrey,     // PLAY EDITOR BACKGROUND
+  color_bgPane: white,         // panel background
+  color_bgMenubar: lavenderGrey, // menubar background color
+  // LOGO ------------------------------
+  logo:[
+    `hsla(${360*Math.random()},100%,90%,1)`,
+    `hsla(${360*Math.random()},100%,90%,1)`,
+    `hsla(${360*Math.random()},100%,90%,1)`,
+  ],
+  // EDITOR ------------------------------
+  color_bgEditor: lavenderGrey,
+  color_bgLinebar: lavenderGrey,
+  color_seperator: smokewhite,
+}
+const darktheme = {
+  // TWM ------------------------------
+  font1: tabfont,        // tab font
+  color_text: slateGrey,       // tab inactive text
+  color_activeText: white,     // tab active text
+  color_bgPanel: dark,          // panel background & separators
+  color_bgTab: darkgrey,      // tab
+  color_bgContent: darkgrey,      // TAB BACKGROUND - lavenderGrey -
+  color_background: slateGrey,     // PLAY EDITOR BACKGROUND
+  color_bgPane: darkgrey,      // panel background
+  color_bgMenubar: darkgrey, // menubar background color
+  // LOGO ------------------------------
+  logo:[
+    `hsla(${360*Math.random()},100%,30%,1)`,
+    `hsla(${360*Math.random()},100%,30%,1)`,
+    `hsla(${360*Math.random()},100%,30%,1)`,
+  ],
+  // EDITOR ------------------------------
+  color_bgEditor: darkgrey,
+  color_bgLinebar: darkgrey,
+  color_seperator: dark,
+}
+
+// module.exports = lighttheme
+module.exports = darktheme
+
+// // ----- CODESANDBOX
+// #1c2022 // dark background
+// #24282a // light background
+// #111314 // seperator
+// #141618 // bar background
+//
+// #333739 // input background
+// #696a6a // input background text
+//
+// #7b7d7e // icons
+//
+// #ffffff // active font
+// #a1a1a2 // inactive font
+// #818183 // counter
+// #434446 // counter circle
+// #65686a //
+//
+// #f2f2f2 // browser bar background
+// #ffffff // browser blank background
+//
+// // ---- CODEPEN
+// #1d1e22 // editor background     !!!!!
+// #31363e // editor line numbers
+// #ffffff // editor text
+//
+// #1a1b1f // menubar backgroundcolor  !!!!!
+// #c5c8d4 // menubar color
+//
+// #26272a // editor-bar seperator
+//
+// #36383f // panel seperator background !!!!!!!
+//
+// #26282d // thin line seperator
+// #080809 // another thin seperator color
+//
+// #36383f // icon background
+// #ffffff // icon text
