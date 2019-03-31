ad178bd0dd1539f05d4093961bf2743f9273af07
ninabreznik <ninabreznik@gmail.com>
2019-03-31 02:56:37 +0100

    Update smartcontract-app

--- bundle.js
+++ bundle.js
@@ -30060,7 +30060,46 @@ var glossary = {
   nonpayable: `NONPAYABLE FUNCTION - function, that changes the state of the contract on the Ethereum network`
 }
 
-},{}],"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/shortenHexData.js":[function(require,module,exports){
+},{}],"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/loadingAnimation.js":[function(require,module,exports){
+const bel = require("bel")
+const csjs = require("csjs-inject")
+
+module.exports = loadingAnimation
+
+function loadingAnimation (colors) {
+  const css = csjs`
+  .loader:before {
+    content: "";
+    position: absolute;
+    width: 4px;
+    height: 4px;
+    top: 45%;
+    left: 80%;
+    background-color: ${colors.whiteSmoke};
+    animation: rotatemove 1s infinite;
+    border-radius: 50%;
+  }
+
+  @keyframes rotatemove {
+    0%{
+      -webkit-transform: scale(1) translateX(0px);
+      -ms-transform: scale(1) translateX(0px);
+      -o-transform: scale(1) translateX(0px);
+      transform: scale(1) translateX(0px);
+    }
+
+    100%{
+      -webkit-transform: scale(2) translateX(45px);
+      -ms-transform: scale(2) translateX(45px);
+      -o-transform: scale(2) translateX(45px);
+      transform: scale(2) translateX(45px);
+    }
+  }`
+  // @TODO: fix theming to not create 10000 style tags for 1000 spinners
+  return bel`<div class=${css.loader}></div>`
+}
+
+},{"bel":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/bel/browser.js","csjs-inject":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/csjs-inject/index.js"}],"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/shortenHexData.js":[function(require,module,exports){
 module.exports = shortenHexData
 
 function shortenHexData (data) {
@@ -30077,6 +30116,7 @@ const ethers = require('ethers')
 const utils = require('ethers').utils
 const glossary = require('glossary')
 const date = require('getDate')
+const loadingAnimation = require('loadingAnimation')
 const getArgs = require('getArgs')
 const shortenHexData = require('shortenHexData')
 const validator = require('solidity-validator')
@@ -30087,7 +30127,6 @@ const inputBoolean = require("input-boolean")
 const inputString = require("input-string")
 const copy = require('copy-text-to-clipboard')
 
-console.log(getArgs)
 // Styling variables
 
 var fonts = [
@@ -30118,7 +30157,7 @@ var colors = {
 var css = csjs`
   @media only screen and (max-width: 3000px) {
     .preview {
-      padding: 5%;
+      padding: 1% 2%;
       min-width: 350px;
       min-height: 100vh;
       font-family: 'Overpass Mono', sans-serif;
@@ -30155,6 +30194,7 @@ var css = csjs`
       height: 0;
     }
     .txReturn {
+      position: relative;
       border: 2px dashed ${colors.darkSmoke};
       border-top: none;
       min-width: 230px;
@@ -30162,7 +30202,6 @@ var css = csjs`
       left: 20px;
       min-height: 80px;
       width: 626px;
-      position: relative;
       display: flex;
       justify-content: center;
       align-items: center;
@@ -30173,6 +30212,7 @@ var css = csjs`
       flex-direction: column;
     }
     .txReturnItem {
+      position: relative;
       font-size: 0.7rem;
       display: flex;
       color: ${colors.whiteSmoke};
@@ -30188,10 +30228,6 @@ var css = csjs`
     .txReturnField {
       display:flex;
       justify-content: flex-start;
-      cursor: pointer;
-    }
-    .txReturnField:hover {
-      opacity: 0.8;
     }
     .txReturnTitle {
       font-weight: bold;
@@ -30199,13 +30235,14 @@ var css = csjs`
     }
     .txReturnValue {
       color: ${colors.whiteSmoke};
+      cursor: pointer;
     }
     .txReturnValue a {
       text-decoration: none;
       color: ${colors.slateGrey};
     }
     .txReturnValue a:hover {
-      opacity: 0.8;
+      opacity: 0.6;
     }
     .contractName {
       cursor: pointer;
@@ -30213,14 +30250,12 @@ var css = csjs`
       font-weight: bold;
       color: ${colors.whiteSmoke};
       margin: 10px 0 20px 10px;
-      min-width: 200px;
-      width: 30%;
       display: flex;
       align-items: end;
     }
     .contractName:hover {
       cursor: pointer;
-      opacity: 0.9;
+      opacity: 0.6;
     }
     .fnName {
       display: flex;
@@ -30275,7 +30310,7 @@ var css = csjs`
     }
     .title:hover {
       cursor: pointer;
-      opacity: 0.9;
+      opacity: 0.6;
     }
     .deployTitle {
       font-size: 1.3rem;
@@ -30291,9 +30326,11 @@ var css = csjs`
       right: -12px;
       font-size: 1.8rem;
       position: absolute;
-      padding: 5 8px;
       background-color: ${colors.dark};
       cursor: pointer;
+    }
+    .deploy:hover {
+      opacity: 0.6;
     }
     .send {
       display: flex;
@@ -30302,13 +30339,67 @@ var css = csjs`
       right: 22px;
       font-size: 2rem;
       position: absolute;
-      color: ${colors.darkSmoke};
       background-color: ${colors.dark};
+      color: ${colors.darkSmoke};
       padding-right: 5px;
     }
     .send:hover {
-      opacity: 0.9;
+      opacity: 0.6;
       cursor: pointer;
+    }
+    .bounce {
+      animation: bounceRight 2s infinite;
+    }
+    @-webkit-keyframes bounceRight {
+      0%,
+      20%,
+      50%,
+      80%,
+      100% {
+        -webkit-transform: translateX(0);
+        transform: translateX(0);
+      }
+      40% {
+        -webkit-transform: translateX(-30px);
+        transform: translateX(-30px);
+      }
+      60% {
+        -webkit-transform: translateX(-15px);
+        transform: translateX(-15px);
+      }
+    }
+    @-moz-keyframes bounceRight {
+      0%,
+      20%,
+      50%,
+      80%,
+      100% {
+        transform: translateX(0);
+      }
+      40% {
+        transform: translateX(-30px);
+      }
+      60% {
+        transform: translateX(-15px);
+      }
+    }
+    @keyframes bounceRight {
+      0%,
+      20%,
+      50%,
+      80%,
+      100% {
+        -ms-transform: translateX(0);
+        transform: translateX(0);
+      }
+      40% {
+        -ms-transform: translateX(-30px);
+        transform: translateX(-30px);
+      }
+      60% {
+        -ms-transform: translateX(-15px);
+        transform: translateX(-15px);
+      }
     }
     .fnContainer {
       position: relative;
@@ -30331,17 +30422,21 @@ var css = csjs`
     .statsEl {
       display:flex;
       justify-content: space-between;
-      cursor: pointer;
+      position: relative;
     }
-    .statsEl:hover {
-        opacity: 0.8;
+    .deployingNotice {
+      margin-right: 5px;
+      font-weight: bold;
     }
     .statsElTitle {
       margin-right: 5px;
       font-weight: bold;
     }
     .statsElValue {
-
+      cursor: pointer;
+    }
+    .statsElValue:hover {
+      opacity: 0.6;
     }
     .inProgress {
     }
@@ -30721,66 +30816,72 @@ function displayContractUI(result) {   // compilation result metadata
       </div>`
     }
 
-    async function sendTx (name, label, e) {
+    async function makeReturn (transaction) {
+      return bel`<div class=${css.txReturnItem}>
+        <div class=${css.returnJSON}>
+          ${JSON.stringify(transaction, null, 1)}
+        </div>
+      </div>`
+    }
+
+    async function makeReceipt (transaction) {
+      let receipt = await transaction.wait()
+      let linkToEtherscan = "https://" + provider._network.name  + ".etherscan.io/tx/" + receipt.transactionHash
+      return bel`<div class=${css.txReturnItem}>
+        <div class=${css.txReturnLeft}>
+          <div class=${css.txReturnField}>
+            <div class=${css.txReturnTitle}>Sent:</div>
+            <div class=${css.txReturnValue}>${date}</div>
+          </div>
+          <div class=${css.txReturnField} onclick=${()=>copy(receipt.transactionHash)}>
+            <div class=${css.txReturnTitle} title="Transaction">Transaction:</div>
+            <div class=${css.txReturnValue}>${shortenHexData(receipt.transactionHash)}</div>
+          </div>
+          <div class=${css.txReturnField} onclick=${()=>copy(receipt.from)}>
+            <div class=${css.txReturnTitle}>Signed by:</div>
+            <div class=${css.txReturnValue}>${shortenHexData(receipt.from)}</div>
+          </div>
+        </div>
+        <div class=${css.txReturnRight} onclick=${()=>copy(transaction.gasPrice._hex)}>
+          <div class=${css.txReturnField}>
+            <div class=${css.txReturnTitle}>Gas price:</div>
+            <div class=${css.txReturnValue}>${parseInt(transaction.gasPrice._hex) || free}</div>
+          </div>
+          <div class=${css.txReturnField} onclick=${()=>copy(receipt.gasUsed._hex)}>
+            <div class=${css.txReturnTitle}>Gas used:</div>
+            <div class=${css.txReturnValue}>${parseInt(receipt.gasUsed._hex)}</div>
+          </div>
+          <div class=${css.txReturnField}>
+            <div class=${css.txReturnTitle}>Details:</div>
+            <div class=${css.txReturnValue}><a href=${linkToEtherscan} target="_blank">Link to Etherscan</a></div>
+          </div>
+        </div>
+      </div>`
+    }
+
+    function makeLoadingAnimation () {
+      return bel`<div class=${css.txReturnItem}>Sending ${loadingAnimation(colors)}</div></div>`
+    }
+
+    async function sendTx (fnName, label, e) {
       let element = e.target.parentNode.parentNode.parentNode.parentNode
       let txReturn = element.querySelector("[class^='txReturn']") || bel`<div class=${css.txReturn}></div>`
-      if (contract) {
-        let fnName = name
+      if (contract) {  // if deployed
         let args = getArgs(element, 'inputContainer')
-        let transaction = await contract.functions[fnName](...args)
-        if (label === 'payable' || label === 'nonpayable') {
-          let receipt = await transaction.wait()
-          let linkToEtherscan = "https://" + provider._network.name  + ".etherscan.io/tx/" + receipt.transactionHash
-          txReturn.appendChild(bel`
-          <div class=${css.txReturnItem}>
-            <div class=${css.txReturnLeft}>
-              <div class=${css.txReturnField}>
-                <div class=${css.txReturnTitle}>Sent:</div>
-                <div class=${css.txReturnValue}>${date}</div>
-              </div>
-              <div class=${css.txReturnField} onclick=${()=>copy(receipt.transactionHash)}>
-                <div class=${css.txReturnTitle} title="Transaction">Transaction:</div>
-                <div class=${css.txReturnValue}>${shortenHexData(receipt.transactionHash)}</div>
-              </div>
-              <div class=${css.txReturnField} onclick=${()=>copy(receipt.from)}>
-                <div class=${css.txReturnTitle}>Signed by:</div>
-                <div class=${css.txReturnValue}>${shortenHexData(receipt.from)}</div>
-              </div>
-            </div>
-            <div class=${css.txReturnRight} onclick=${()=>copy(transaction.gasPrice._hex)}>
-              <div class=${css.txReturnField}>
-                <div class=${css.txReturnTitle}>Gas price:</div>
-                <div class=${css.txReturnValue}>${parseInt(transaction.gasPrice._hex) || free}</div>
-              </div>
-              <div class=${css.txReturnField} onclick=${()=>copy(receipt.gasUsed._hex)}>
-                <div class=${css.txReturnTitle}>Gas used:</div>
-                <div class=${css.txReturnValue}>${parseInt(receipt.gasUsed._hex)}</div>
-              </div>
-              <div class=${css.txReturnField}>
-                <div class=${css.txReturnTitle}>Details:</div>
-                <div class=${css.txReturnValue}><a href=${linkToEtherscan} target="_blank">Link to Etherscan</a></div>
-              </div>
-            </div>
-          </div>`)
-          element.appendChild(txReturn)
-        }
-        if (label === 'pure' || label === 'view') {
-          txReturn.innerHTML = `
-            <div class=${css.txReturnItem}>
-              <div class=${css.returnJSON}>
-                ${JSON.stringify(transaction, null, 1)}
-              </div>
-            </div>`
-          element.appendChild(txReturn)
-        }
+        let signer = await provider.getSigner()
+        let contractAsCurrentSigner = contract.connect(signer)
+        let transaction = await contractAsCurrentSigner.functions[fnName](...args)
+        // @TODO: what if users cancels a transaction?
+        element.appendChild(txReturn)
+        var loader = makeLoadingAnimation()
+        txReturn.appendChild(loader)
+        if (label === 'payable' || label === 'nonpayable') var el = await makeReceipt(transaction)
+        if (label === 'pure' || label === 'view') var el = await makeReturn(transaction)
+        loader.replaceWith(el)
       } else {
         let deploy = document.querySelector("[class^='deploy']")
-        setTimeout(()=>{deploy.style.color = colors.darkSmoke}, 1000)
-        setTimeout(()=>{deploy.style.color = colors.whiteSmoke}, 1500)
-        setTimeout(()=>{deploy.style.color = colors.darkSmoke}, 2000)
-        setTimeout(()=>{deploy.style.color = colors.whiteSmoke}, 2500)
-        setTimeout(()=>{deploy.style.color = colors.darkSmoke}, 3000)
-        setTimeout(()=>{deploy.style.color = colors.whiteSmoke}, 3500)
+        deploy.classList.add(css.bounce)
+        setTimeout(()=>deploy.classList.remove(css.bounce), 3500)
       }
     }
 
@@ -30875,13 +30976,15 @@ function displayContractUI(result) {   // compilation result metadata
 
     function deployingNotice() {
       let txReturn = document.querySelector("[class^='txReturn']")
+      let loading = bel`<div class=${css.inProgress}></div>`
       ctor.innerHTML = `
         <div class=${css.deployStats}>
           <div class=${css.statsEl}>
-            <div class=${css.statsElTitle}>Deploying to Ethereum network</div>
-            <div class=${css.inProgress}>...</div>
+            <div class=${css.deployingNotice}>Deploying to Ethereum network</div>
           </div>
         </div>`
+      ctor.appendChild(loading)
+      loading.appendChild(loadingAnimation(colors))
       if (txReturn) txReturn.parentNode.removeChild(txReturn)
     }
 
@@ -30950,7 +31053,7 @@ function displayContractUI(result) {   // compilation result metadata
   }
 }
 
-},{"bel":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/bel/browser.js","copy-text-to-clipboard":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/copy-text-to-clipboard/index.js","csjs-inject":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/csjs-inject/index.js","ethers":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/ethers/dist/ethers.min.js","getArgs":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/getArgs.js","getDate":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/getDate.js","glossary":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/glossary.js","input-address":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/input-address/src/input-address.js","input-array":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/input-array/src/input-array.js","input-boolean":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/input-boolean/src/input-boolean.js","input-integer":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/input-integer/src/input-integer.js","input-string":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/input-string/src/input-string.js","shortenHexData":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/shortenHexData.js","solidity-validator":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/solidity-validator/src/index.js"}],"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/solc-import/src/combineSource.js":[function(require,module,exports){
+},{"bel":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/bel/browser.js","copy-text-to-clipboard":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/copy-text-to-clipboard/index.js","csjs-inject":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/csjs-inject/index.js","ethers":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/ethers/dist/ethers.min.js","getArgs":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/getArgs.js","getDate":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/getDate.js","glossary":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/glossary.js","input-address":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/input-address/src/input-address.js","input-array":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/input-array/src/input-array.js","input-boolean":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/input-boolean/src/input-boolean.js","input-integer":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/input-integer/src/input-integer.js","input-string":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/input-string/src/input-string.js","loadingAnimation":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/loadingAnimation.js","shortenHexData":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/smartcontract-app/src/node_modules/shortenHexData.js","solidity-validator":"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/solidity-validator/src/index.js"}],"/home/ninabreznik/Documents/code/fairyDust/ethereum/play/play-editor/node_modules/solc-import/src/combineSource.js":[function(require,module,exports){
 const getImports = require('./getImports');
 const isExistImport = require('./isExistImport');
 
