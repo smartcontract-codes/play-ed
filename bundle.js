d05f3ab7e2324e42e424b7b3d9a83ea80eb9036d
ninabreznik <ninabreznik@gmail.com>
2019-04-08 12:06:36 +0100

    Add simple code

--- bundle.js
+++ bundle.js
@@ -33,86 +33,24 @@ document.head.appendChild(style)
 
 function contract () {
   return `
-  pragma solidity >=0.5.0;
-  pragma experimental ABIEncoderV2;
-  contract InvoiceJournal {
-    struct Contractor {
-      string name;
-      string email;
-      string pubkey;
-      bool active;
-      bool exists;
+pragma solidity ^0.5.2;
+
+contract HelloWorld {
+
+    uint8 public num;
+
+    event NewNum(address indexed _who, uint8 _newNum, uint _timestamp);
+
+    constructor(uint8 _num) public {
+        num = _num;
     }
-    struct Invoice {
-      address contractor;
-      uint invoice_id;
-      string storage_url;
-      string[] encrypted_decrypt_keys; // @TODO: not in use yet :-)
+
+    function setNum(uint8 _newNum) public {
+        num = _newNum;
+        emit NewNum(msg.sender, _newNum, now);
     }
-    address operator;
-    mapping(address => Contractor) contractors;
-    mapping(address => Invoice[]) invoices;
-    address[] contractor_addresses;
-    function getAllInvoices () public view returns (Invoice[][] memory) {
-      uint len = contractor_addresses.length;
-    	Invoice[][] memory result = new Invoice[][](len);
-      for (uint i = 0; i < len; i++) {
-        result[i] = invoices[contractor_addresses[i]];
-      }
-      return result;
-    }
-    function getAllContractors () public view returns (Contractor[] memory) {
-      uint len = contractor_addresses.length;
-    	Contractor[] memory result = new Contractor[](len);
-      for (uint i = 0; i < len; i++) {
-        result[i] = contractors[contractor_addresses[i]];
-      }
-      return result;
-    }
-    function getYourInvoices () public view returns (Invoice[] memory) {
-      return invoices[msg.sender];
-    }
-    function activateContractor (address contractor_address) public {
-      require(operator == msg.sender, "Only an authorized operator can add new contractors");
-      Contractor storage contractor = contractors[contractor_address];
-      contractor.active = true;
-      if (!contractor.exists) {
-        contractor.exists = true;
-        contractor_addresses.push(contractor_address);
-      }
-    }
-    function deactivateContractor (address contractor_address) public {
-      require(operator == msg.sender, "Only an authorized operator can remove contractors");
-      Contractor storage contractor = contractors[contractor_address];
-      if (!contractor.active) return;
-      contractor.active = false;
-    }
-    function updateContractor (string memory name, string memory email, string memory pubkey) public {
-      Contractor storage contractor = contractors[msg.sender];
-      require(contractor.active, "Unauthorized contractors cannot set their pubkeys");
-      contractor.name = name;
-      contractor.email = email;
-      contractor.pubkey = pubkey;
-    }
-    function addInvoice (uint invoice_id, string memory storage_url, string[] memory keys) public returns (Contractor memory) {
-      Contractor memory contractor = contractors[msg.sender];
-      require(contractor.exists, "Unknown contractors cannot submit invoices");
-      require(contractor.active, "Unauthorized contractors cannot submit invoices");
-      Invoice[] storage _invoices = invoices[msg.sender];
-      Invoice memory new_invoice = Invoice({
-        contractor: msg.sender,
-        invoice_id: invoice_id,
-        storage_url: storage_url,
-        encrypted_decrypt_keys: keys
-      });
-      _invoices.push(new_invoice);
-      return contractor;
-    }
-    constructor () public {
-      operator = msg.sender;
-    }
-  }
-  `
+}
+`
 }
 
 },{"../":"/home/ninabreznik/Documents/code/ethereum/play/play-editor/src/play-editor.js"}],"/home/ninabreznik/Documents/code/ethereum/play/play-editor/node_modules/@babel/runtime/helpers/typeof.js":[function(require,module,exports){
@@ -33957,105 +33895,22 @@ module.exports = playeditor
 
 function playeditor (opts = {}, theme = defaultTheme) {
   const code = `
-pragma solidity >=0.5.0;
-pragma experimental ABIEncoderV2;
+pragma solidity ^0.5.2;
 
-contract InvoiceJournal {
+contract HelloWorld {
 
-/// WHAT DATA WILL WE STORE ON BLOCKCHAIN
-  struct Contractor {
-    string name;
-    string email;
-    string pubkey;
-    bool active;
-    bool exists;
-  }
-  struct Invoice {
-    address contractor;
-    uint invoice_id;
-    string storage_url;
-    string[] encrypted_decrypt_keys; // @TODO: not in use yet :-)
-  }
-  address operator;
-  mapping(address => Contractor) contractors;
-  mapping(address => Invoice[]) invoices;
-  address[] contractor_addresses;
+    uint8 public num;
 
-/// GET A LIST OF ALL PUBLISHED INVOICES
-  function getAllInvoices () public view returns (Invoice[][] memory) {
-    uint len = contractor_addresses.length;
-  	Invoice[][] memory result = new Invoice[][](len);
-    for (uint i = 0; i < len; i++) {
-      result[i] = invoices[contractor_addresses[i]];
+    event NewNum(address indexed _who, uint8 _newNum, uint _timestamp);
+
+    constructor(uint8 _num) public {
+        num = _num;
     }
-    return result;
-  }
 
-/// GET A LIST OF ALL CONTRACTORS (ACTIVE AND NOT ACTIVE)
-  function getAllContractors () public view returns (Contractor[] memory) {
-    uint len = contractor_addresses.length;
-  	Contractor[] memory result = new Contractor[](len);
-    for (uint i = 0; i < len; i++) {
-      result[i] = contractors[contractor_addresses[i]];
+    function setNum(uint8 _newNum) public {
+        num = _newNum;
+        emit NewNum(msg.sender, _newNum, now);
     }
-    return result;
-  }
-
-/// GET A LIST OF ALL YOUR INVOICES
-  function getYourInvoices () public view returns (Invoice[] memory) {
-    return invoices[msg.sender];
-  }
-
-/// FIRST STEP: ADD/RE-ACTIVATE A CONTRACTOR
-  function activateContractor (address contractor_address) public {
-    require(operator == msg.sender, "Only an authorized operator can add new contractors");
-    Contractor storage contractor = contractors[contractor_address];
-    contractor.active = true;
-    if (!contractor.exists) {
-      contractor.exists = true;
-      contractor_addresses.push(contractor_address);
-    }
-  }
-
-/// DE-ACTIVATE A CONTRACTOR
-  function deactivateContractor (address contractor_address) public {
-    require(operator == msg.sender, "Only an authorized operator can remove contractors");
-    Contractor storage contractor = contractors[contractor_address];
-    if (!contractor.active) return;
-    contractor.active = false;
-  }
-
-/// ONLY CONTRACTORS THEMSELF CAN UPDATE THEIR DATA
-  function updateContractor (string memory name, string memory email, string memory pubkey) public {
-    Contractor storage contractor = contractors[msg.sender];
-    require(contractor.active, "Unauthorized contractors cannot set their pubkeys");
-    contractor.name = name;
-    contractor.email = email;
-    contractor.pubkey = pubkey;
-  }
-
-/// ACTIVE CONTRACTOR CAN ADD A NEW INVOICE
-  function addInvoice (uint invoice_id, string memory storage_url, string[] memory keys) public returns (Contractor memory) {
-    Contractor memory contractor = contractors[msg.sender];
-    require(contractor.exists, "Unknown contractors cannot submit invoices");
-    require(contractor.active, "Unauthorized contractors cannot submit invoices");
-    Invoice[] storage _invoices = invoices[msg.sender];
-    Invoice memory new_invoice = Invoice({
-      contractor: msg.sender,
-      invoice_id: invoice_id,
-      storage_url: storage_url,
-      encrypted_decrypt_keys: keys
-    });
-
-    _invoices.push(new_invoice);
-    return contractor;
-  }
-
-/// CONSTRUCTOR (RUNS ONLY ONCE - WHEN CONTRACT IS DEPLOYED)
-  constructor () public {
-    operator = msg.sender;
-  }
-
 }
 `
   const ed = {
--- demo/demo.js
+++ demo/demo.js
@@ -32,84 +32,22 @@ document.head.appendChild(style)
 
 function contract () {
   return `
-  pragma solidity >=0.5.0;
-  pragma experimental ABIEncoderV2;
-  contract InvoiceJournal {
-    struct Contractor {
-      string name;
-      string email;
-      string pubkey;
-      bool active;
-      bool exists;
+pragma solidity ^0.5.2;
+
+contract HelloWorld {
+
+    uint8 public num;
+
+    event NewNum(address indexed _who, uint8 _newNum, uint _timestamp);
+
+    constructor(uint8 _num) public {
+        num = _num;
     }
-    struct Invoice {
-      address contractor;
-      uint invoice_id;
-      string storage_url;
-      string[] encrypted_decrypt_keys; // @TODO: not in use yet :-)
+
+    function setNum(uint8 _newNum) public {
+        num = _newNum;
+        emit NewNum(msg.sender, _newNum, now);
     }
-    address operator;
-    mapping(address => Contractor) contractors;
-    mapping(address => Invoice[]) invoices;
-    address[] contractor_addresses;
-    function getAllInvoices () public view returns (Invoice[][] memory) {
-      uint len = contractor_addresses.length;
-    	Invoice[][] memory result = new Invoice[][](len);
-      for (uint i = 0; i < len; i++) {
-        result[i] = invoices[contractor_addresses[i]];
-      }
-      return result;
-    }
-    function getAllContractors () public view returns (Contractor[] memory) {
-      uint len = contractor_addresses.length;
-    	Contractor[] memory result = new Contractor[](len);
-      for (uint i = 0; i < len; i++) {
-        result[i] = contractors[contractor_addresses[i]];
-      }
-      return result;
-    }
-    function getYourInvoices () public view returns (Invoice[] memory) {
-      return invoices[msg.sender];
-    }
-    function activateContractor (address contractor_address) public {
-      require(operator == msg.sender, "Only an authorized operator can add new contractors");
-      Contractor storage contractor = contractors[contractor_address];
-      contractor.active = true;
-      if (!contractor.exists) {
-        contractor.exists = true;
-        contractor_addresses.push(contractor_address);
-      }
-    }
-    function deactivateContractor (address contractor_address) public {
-      require(operator == msg.sender, "Only an authorized operator can remove contractors");
-      Contractor storage contractor = contractors[contractor_address];
-      if (!contractor.active) return;
-      contractor.active = false;
-    }
-    function updateContractor (string memory name, string memory email, string memory pubkey) public {
-      Contractor storage contractor = contractors[msg.sender];
-      require(contractor.active, "Unauthorized contractors cannot set their pubkeys");
-      contractor.name = name;
-      contractor.email = email;
-      contractor.pubkey = pubkey;
-    }
-    function addInvoice (uint invoice_id, string memory storage_url, string[] memory keys) public returns (Contractor memory) {
-      Contractor memory contractor = contractors[msg.sender];
-      require(contractor.exists, "Unknown contractors cannot submit invoices");
-      require(contractor.active, "Unauthorized contractors cannot submit invoices");
-      Invoice[] storage _invoices = invoices[msg.sender];
-      Invoice memory new_invoice = Invoice({
-        contractor: msg.sender,
-        invoice_id: invoice_id,
-        storage_url: storage_url,
-        encrypted_decrypt_keys: keys
-      });
-      _invoices.push(new_invoice);
-      return contractor;
-    }
-    constructor () public {
-      operator = msg.sender;
-    }
-  }
-  `
+}
+`
 }
