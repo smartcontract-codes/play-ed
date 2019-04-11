c55af79857e262e47f987749f4daa5b94fcf5a29
ninabreznik <ninabreznik@gmail.com>
2019-04-11 12:17:34 +0100

    Update smartcontract-app width again

--- bundle.js
+++ bundle.js
@@ -30156,7 +30156,7 @@ var css = csjs`
       top: -41px;
       left: 20px;
       min-height: 80px;
-      width: 546px;
+      width: 550px;
       display: flex;
       justify-content: center;
       align-items: center;
